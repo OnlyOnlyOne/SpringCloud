e663ec47b3f57df04194431fba93cc7510404ce7
only <hwy113329@163.com>
2022-03-21 18:50:57 +0800

    查询目录

---
+++ code/leyou/leyou-gateway/src/main/java/com/leyou/gateway/config/LeyouCorsConfiguration.java
@@ -0,0 +1,29 @@
+package com.leyou.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class LeyouCorsConfiguration {
+
+    @Bean
+    public CorsFilter corsFilter() {
+
+        //初始化cors配置对象
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        corsConfiguration.addAllowedOrigin("http://manage.leyou.com");
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource configurationSource = new UrlBasedCorsConfigurationSource();
+        configurationSource.registerCorsConfiguration("/**", corsConfiguration);
+
+        //返回corsFilter实例，参数：cors配置源对象
+        return new CorsFilter(configurationSource);
+    }
+}
---
+++ code/leyou/leyou-item/leyou-item-service/src/main/java/com/leyou/item/service/CategoryService.java
@@ -0,0 +1,28 @@
+package com.leyou.item.service;
+
+import com.leyou.item.mapper.CategoryMapper;
+import com.leyou.item.pojo.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryMapper categoryMapper;
+
+
+    /**
+     * Query categories by pid list.
+     * 根据父节点的id查询子节点
+     * @param pid the pid
+     * @return the list
+     */
+    public List<Category> queryCategoriesByPid(Long pid) {
+        Category record = new Category();
+        record.setParentId(pid);
+        return this.categoryMapper.select(record);
+    }
+}
